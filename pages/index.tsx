f2298ea0caf10d0d25df22a7b6b8d50bf9558fb7
metaclep <cleepe5@hotmail.com>
2022-04-05 19:17:24 -0300

    rota API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-const Home = () => {
-  return <div></div>;
+import { GetServerSideProps } from 'next';
+import { getAllTodos, Todo } from '../lib/db';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const todos = await getAllTodos();
+  return {
+    props: {
+      todos,
+    },
+  };
+};
+
+interface PostProps {
+  todos: Todo[];
+}
+
+const Home = ({ todos }: PostProps) => {
+  return <div>{JSON.stringify(todos, null, 4)}</div>;
 };
 
 export default Home;
